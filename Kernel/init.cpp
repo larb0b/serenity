b912b6d11195a29018d55345495c99a4a05595d5
Larkin Nickle <me@larbob.org>
2019-09-26 14:31:02 -0400

    init: Disable graphical start

--- Kernel/init.cpp
+++ Kernel/init.cpp
@@ -145,7 +145,7 @@ VFS* vfs;
     // SystemServer will start WindowServer, which will be doing graphics.
     // From this point on we don't want to touch the VGA text terminal or
     // accept keyboard input.
-    tty0->set_graphical(true);
+    tty0->set_graphical(false);
 
     auto* system_server_process = Process::create_user_process("/bin/SystemServer", (uid_t)0, (gid_t)0, (pid_t)0, error, {}, {}, tty0);
     if (error != 0) {
